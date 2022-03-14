03eda6a781cce67cbddf0a15466755dd78486e6d
Mateusz Pawłowicz <mpawlowi@vnode-01.dydaktyka.icsr.agh.edu.pl>
2022-03-14 19:43:20 +0100

    add ex1 from lab1

--- lab1/ex1_r.c
+++ lab1/ex1_r.c
@@ -17,14 +17,15 @@ int main(int argc, char** argv) {
   }
   int number;
   if (world_rank == 0) {
+    sleep(2);
     printf("Process 0 - sending!\n");
+    
     // If we are rank 0, set the number to -1 and send it to process 1
     number = -1;
-    MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    MPI_Rsend(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
     printf("Process 0 - sending over!\n");
   } else if (world_rank == 1) {
     printf("Process 1 - started!\n");
-    sleep(5);
     MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     printf("Process 1 received number %d from process 0\n", number);
   }
