d94e725a06c457b934d430e729a97c53f48248d4
Observer46 <microplayerpl@gmail.com>
2022-04-27 12:04:13 +0200

    testing parameters

---
+++ lab4/launch_sorting_50_2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH --nodes 1
+#SBATCH --ntasks 12
+#SBATCH --time=00:25:00
+#SBATCH --partition=plgrid-short
+#SBATCH --account=plgmpr22
+#SBATCH --sockets-per-node=2
+gcc -fopenmp -o bucket.out bucket_sort_3_50_2.c
+for attempt in {1..10}
+do
+    for proc in {1..12}
+    do 
+        ./bucket.out ${proc} 100000000 --verbose
+    done
+done
